7286969022f3086b68d32a846c7964ecab4e412a
wjyzxcv <wjyzxcv@gmail.com>
2022-09-09 11:03:42 +0900

    clean up

--- web_randomizer.py
+++ web_randomizer.py
@@ -5,6 +5,10 @@ from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
+def pd_df2html(df):
+    return df.to_html(classes=["table table-bordered table-striped table-hover"],index=False, justify = "center").replace('<tr>', '<tr align="center">')
+
+
 def dom_ran(mode, expan="0", n_cards="2"):
     n_cards = int(n_cards)
     expan = int(expan)
@@ -19,13 +23,13 @@ def dom_ran(mode, expan="0", n_cards="2"):
     df_name_sets.columns = ["Set"]
     if mode == "a":
         n_req = n_cards
-        return pd.DataFrame(np.random.choice(name_sets, n_req, replace = False), columns=["Expansions"]).to_html(classes=["table table-bordered table-striped table-hover"], index=False, justify = "center").replace('<tr>', '<tr align="center">')
+        return pd_df2html(pd.DataFrame(np.random.choice(name_sets, n_req, replace = False), columns=["Expansions"]))
     elif mode == "b":
         set_ind = expan
         set_name = df_name_sets.iloc[set_ind,0]
         l_cards = d_table[d_table["Set"] == set_name]["Name"]
         n_cards = n_cards
-        return l_cards.sample(n_cards).to_frame().to_html(classes=["table table-bordered table-striped table-hover"],index=False, justify = "center").replace('<tr>', '<tr align="center">')
+        return pd_df2html(l_cards.sample(n_cards).to_frame())
     elif mode == "c":
         event_table = pd.read_sql('''
                                 SELECT *
@@ -37,35 +41,35 @@ def dom_ran(mode, expan="0", n_cards="2"):
         l_events = event_table[event_table["Set"] == set_name]["Name"]
         l_events = l_events.to_frame()
         n_events = n_cards
-        return l_events.sample(n_events).to_html(classes=["table table-bordered table-striped table-hover"],index=False, justify = "center").replace('<tr>', '<tr align="center">')
+        return pd_df2html(l_events.sample(n_events))
     elif mode == "d":
         lm_table = pd.read_sql('''
                                 SELECT *
                                 FROM lm_table
                                 ''', sq_con)
         n_lms = n_cards
-        return lm_table["Name"].to_frame().sample(n_lms).to_html(classes=["table table-bordered table-striped table-hover"],index=False, justify = "center").replace('<tr>', '<tr align="center">')
+        return pd_df2html(lm_table["Name"].to_frame().sample(n_lms))
     elif mode == "e":
         pj_table = pd.read_sql('''
                                 SELECT *
                                 FROM pj_table
                                 ''', sq_con)
         n_pjs = n_cards
-        return pj_table["Name"].to_frame().sample(n_pjs).to_html(classes=["table table-bordered table-striped table-hover"],index=False, justify = "center").replace('<tr>', '<tr align="center">')
+        return pd_df2html(pj_table["Name"].to_frame().sample(n_pjs))
     elif mode == "f":
         way_table = pd.read_sql('''
                                 SELECT *
                                 FROM way_table
                                 ''', sq_con)
         n_ways = n_cards
-        return way_table["Name"].to_frame().sample(n_ways).to_html(classes=["table table-bordered table-striped table-hover"],index=False, justify = "center").replace('<tr>', '<tr align="center">')
+        return pd_df2html(way_table["Name"].to_frame().sample(n_ways))
     elif mode == "g":
         ally_table = pd.read_sql('''
                                 SELECT *
                                 FROM ally_table
                                 ''', sq_con)
         n_allies = n_cards
-        return ally_table["Name"].to_frame().sample(n_allies).to_html(classes=["table table-bordered table-striped table-hover"],index=False, justify = "center").replace('<tr>', '<tr align="center">')
+        return pd_df2html(ally_table["Name"].to_frame().sample(n_allies))
     elif mode == "ini":
         return df_name_sets["Set"].to_list()
 
